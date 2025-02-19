960fc8e090c2db515650da8e98545ec36f76b099
your-username <your-email@example.com>
2025-02-19 17:35:52 +0100

    first day-start of the project

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { navlink } from "../data/dummydata";
+import logo from "../data/images/loggo.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container flexsb">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="nav">
+          {navlink.map((links, i) => (
+            <Link to={links.url} key={i}>
+              {links.text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </header>
+  );
+};
